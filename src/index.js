bd30d1989ab131adbe537ab265b7d78fd361df6f
Moulesh <mouleshchavan@gmail.com>
2022-04-11 20:34:21 +0530

    Mongo Connect

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const mongoose=require("mongoose")
+
 
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://Mouleshchavan:YpmtEPmAjBeUCOGG@cluster0.r5obc.mongodb.net/Mouleshchavandb",    {
+    maxPoolSize: 50, 
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+})
+.then( () => console.log("Mongodb is connected"))
+.catch( err => console.log(err))
+
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -40,11 +40,11 @@ let players =
        let data = req.body
        console.log(data)
        players.map((x) => {
-           if (x.name == data.name) {
+           if (x.name == data.name) 
                return res.send("player already exist")
            }
    
-       })
+       )
        players.push(data)
        res.send({ msg: "My NewPlayerList:" , Player : players })
 

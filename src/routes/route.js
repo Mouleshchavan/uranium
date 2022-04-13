0db7bb61fe413e4211075eab68cf3ff57af804db
Moulesh <mouleshchavan@gmail.com>
2022-04-13 23:29:05 +0530

    Msg

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,53 @@
+const { request } = require('express');
 const express = require('express');
-
 const router = express.Router();
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+       //LOGIC WILL COME HERE
+   
+       let data = req.body
+       console.log(data)
+       players.map((x) => {
+           if (x.name == data.name) {
+               return res.send("player allready esxist")
+           }
+   
+       })
+       players.push(data)
+       res.send({ msg: "hi guys..my 2nd post req" , newPlayer : players })
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
-
+   })
+  
 module.exports = router;
-// adding this comment for no reason

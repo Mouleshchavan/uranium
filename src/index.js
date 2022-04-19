96ce88cc80bc635c826797d3a9df4032361628d4
Moulesh <mouleshchavan@gmail.com>
2022-04-20 01:09:17 +0530

    Assignment Middleware

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const mongoose=require("mongoose")
+
+
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Mouleshchavan:YpmtEPmAjBeUCOGG@cluster0.r5obc.mongodb.net/Mouleshchavan3-db",    {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.then( () => console.log("Mongodb is connected"))
+.catch( err => console.log(err))
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use(
+    function(req,res,next){
+        let ar1=new Date().toLocaleString();
+        let ar2=req.ip
+        console.log(ar1,ar2,"/Moulesh")
+        next()
+    }
+)
+
 
 app.use('/', route);
 
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+
